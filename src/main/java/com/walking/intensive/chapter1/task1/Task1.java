ec1179dc6f32ec76c56f4e386586ac4c4b51f3d0
inwizard <inwiz@mail.ru>
2024-11-17 18:52:51 +0300

    task1

--- src/main/java/com/walking/intensive/chapter1/task1/Task1.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Task1.java
@@ -15,14 +15,40 @@ package com.walking.intensive.chapter1.task1;
 public class Task1 {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-        int age = 0;
+        int age = 33;
+
 
         System.out.println(getAgeString(age));
     }
 
     static String getAgeString(int age) {
 //        Место для вашего кода
+        if (age < 0 || age > 127) {
+            System.out.println("Некорректный ввод");
+            return null;
+        }
 
-        return null; // Заглушка. При реализации - удалить
+
+        int lastCharacter = age;
+
+
+        if (age > 9) {
+            lastCharacter = age % 10;
+        }
+
+        String endWord = switch (lastCharacter) {
+            case (0) -> "лет";
+            case (1) -> "год";
+            case (2) -> "года";
+            case (3) -> "года";
+            case (4) -> "года";
+            default -> "лет";
+        };
+        if (age > 10 && age < 20 || age > 110 && age < 120) {
+            endWord = "лет";
+        }
+
+
+        return "Вам " + age + " " + endWord;
     }
 }
